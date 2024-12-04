b2143fbdb168eedb0a843750f826de10ff9e10c3
Angela <alcoutland@outlook.com>
2024-12-03 19:29:35 -0500

    add index file

---
+++ my-app/js/script.js
@@ -0,0 +1,8 @@
+// Display a welcome message in the browser console
+console.log("Welcome to my webpage!");
+
+// Change the text color of an element
+function changeTextColor() {
+    const heading = document.querySelector("h1");
+    heading.style.color = "blue";
+}
